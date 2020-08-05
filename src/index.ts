2b6693da51c57b8700ea97c2522552915d14a668
Connor White <connor.bcw@gmail.com>
2020-08-05 18:54:31 -0500

    Better type support, support for ListValue argument types

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,179 @@
 import gql from "graphql-tag";
 import { jsonToGraphQLQuery, VariableType, EnumType } from "json-to-graphql-query";
 
+type Variables = {
+  [name: string]: string | undefined;
+}
+
+type ASTNode = {
+  kind: string;
+  loc: Location;
+}
+
+type Location = {
+  start: number;
+  end: number;
+  startToken: LocationToken;
+  endToken: LocationToken;
+  source: LocationSource;
+}
+
+type LocationOffset = {
+  line: number;
+  column: number;
+}
+
+type LocationToken = {
+  kind: string;
+  start: number;
+  end: number;
+  value: string | undefined;
+  prev: LocationToken;
+  next: LocationToken;
+} & LocationOffset;
+
+type LocationSource = {
+  body: string;
+  name: string;
+  locationOffset: LocationOffset;
+}
+
+interface Document extends ASTNode {
+  kind: "Document";
+  definitions: Definition[];
+}
+
+type Definition = OperationDefinition | FragmentDefinition;
+
+interface OperationDefinition extends ASTNode {
+  kind: "OperationDefinition";
+  operation: string;
+  name: Name;
+  variableDefinitions: VariableDefinition[];
+  directives: Directive[];
+  selectionSet: SelectionSet;
+}
+
+interface FragmentDefinition extends ASTNode {
+  kind: "FragmentDefinition";
+  name: Name;
+  typeCondition: NamedType;
+  directives: Directive[];
+  selectionSet: SelectionSet;
+}
+
+interface VariableDefinition extends ASTNode {
+  kind: "VariableDefinition";
+  variable: Variable;
+  type: Type;
+  defaultValue: Value;
+  directives: Directive[];
+}
+
+type Directive = any;
+
+interface Variable extends ASTNode {
+  kind: "Variable";
+  name: Name;
+}
+
+interface Name extends ASTNode {
+  kind: "Name";
+  value: string;
+}
+
+type Type = NamedType | NonNullType;
+
+interface NamedType extends ASTNode {
+  kind: "NamedType";
+  name: Name;
+}
+
+interface NonNullType extends ASTNode {
+  kind: "NonNullType";
+  type: NamedType;
+}
+
+interface SelectionSet extends ASTNode {
+  kind: "SelectionSet";
+  selections: (Field | FragmentSpread)[];
+}
+
+interface FragmentSpread extends ASTNode {
+  kind: "FragmentSpread";
+  name: Name;
+  directives: any[];
+  loc: Location;
+}
+
+interface Field extends ASTNode {
+  kind: "Field";
+  alias: Name | undefined;
+  name: Name;
+  arguments: Argument[];
+  directives: any[];
+  selectionSet: SelectionSet | undefined;
+}
+
+interface Argument extends ASTNode {
+  kind: "Argument";
+  name: Name;
+  value: Value;
+}
+
+interface StringValue extends ASTNode {
+  kind: "StringValue";
+  value: string;
+  block: boolean;
+}
+
+interface ObjectValue extends ASTNode {
+  kind: "ObjectValue";
+  fields: ObjectField[];
+}
+
+interface ObjectField extends ASTNode {
+  kind: "ObjectField";
+  name: Name;
+  value: Value;
+}
+
+interface BooleanValue extends ASTNode {
+  kind: "BooleanValue";
+  value: boolean;
+}
+
+interface IntValue extends ASTNode {
+  kind: "IntValue";
+  value: string;
+}
+
+interface EnumValue extends ASTNode {
+  kind: "EnumValue";
+  value: string;
+}
+
+interface ListValue extends ASTNode {
+  kind: "ListValue";
+  values: Value[];
+}
+
+type Value = StringValue | ObjectValue | BooleanValue | IntValue | Variable | EnumValue | ListValue;
+
 const isEmpty = (object: any) => {
   return Object.keys(object).length === 0;
 };
 
-const getVariableType = (definition: any) => {
-  if(definition.type.name) {
+const getVariableType = (definition: VariableDefinition) => {
+  if(definition.type.kind === "NamedType") {
     return definition.type.name.value;
   } else {
-    return `${definition.type.type.name.value}${definition.type.kind === "NonNullType" ? "!" : ""}`;
+    return `${definition.type.type.name.value}!`;
   }
 };
 
-const getVariables = (variableDefinitions: any[]=[], variables: {}) => {
-  let json = {};
+const getVariables = (variableDefinitions: VariableDefinition[], variables: Variables) => {
+  let json: Variables = {};
   variableDefinitions.forEach((definition) => {
     if(variables[definition.variable.name.value] !== undefined) {
       json[definition.variable.name.value] = getVariableType(definition);
@@ -23,29 +182,38 @@ const getVariables = (variableDefinitions: any[]=[], variables: {}) => {
   return json;
 };
 
-const getArgs = (args: any[]=[], variables: {}) => {
-  let json = {};
+const getArgValue = (value: Value, variables: Variables): any => {
+  if(value.kind === "ObjectValue") {
+    const val = getArgs(value.fields, variables);
+    return !isEmpty(val) ? val : undefined;
+  } else if(value.kind === "Variable" && variables[value.name.value] !== undefined) {
+    return new VariableType(value.name.value);
+  } else if(value.kind === "StringValue") {
+    return value.value;
+  } else if(value.kind === "IntValue") {
+    return parseInt(value.value);
+  } else if(value.kind === "EnumValue") {
+    return new EnumType(value.value);
+  } else if(value.kind === "ListValue") {
+    return value.values.map((val) => getArgValue(val, variables));
+  } else {
+    return undefined;
+  }
+}
+
+const getArgs = (args: (Argument | ObjectField)[]=[], variables: Variables) => {
+  let json: any = {};
   args.forEach((arg) => {
-    if(arg.value.kind === "ObjectValue" || arg.value.kind === "ObjectField") {
-      const value = getArgs(arg.value.fields, variables);
-      if(!isEmpty(value)) {
-        json[arg.name.value] = value;
-      }
-    } else if(arg.value.kind === "Variable" && variables[arg.value.name.value] !== undefined) {
-      json[arg.name.value] = new VariableType(arg.value.name.value);
-    } else if(arg.value.kind === "IntValue") {
-      json[arg.name.value] = parseInt(arg.value.value);
-    } else if(arg.value.kind === "StringValue") {
-      json[arg.name.value] = arg.value.value;
-    } else if(arg.value.kind === "EnumValue") {
-      json[arg.name.value] = new EnumType(arg.value.value);
+    const value = getArgValue(arg.value, variables);
+    if(value !== undefined) {
+      json[arg.name.value] = value;
     }
   });
   return json;
 }
 
-const getField = (selection: any, variables: {}) => {
-  let json = {};
+const getField = (selection: Field, variables: Variables) => {
+  let json: any = {};
   if(selection.selectionSet !== undefined) {
     json = getSelections(selection.selectionSet.selections, variables);
   }
@@ -59,8 +227,8 @@ const getField = (selection: any, variables: {}) => {
   return json;
 };
 
-const getSelections = (selections: any[]=[], variables: {}) => {
-  let json = {};
+const getSelections = (selections: (Field | FragmentSpread)[] = [], variables: Variables) => {
+  let json: any = {};
   selections.forEach((selection) => {
     if(selection.kind === "Field") {
       json[selection.alias ? selection.alias.value : selection.name.value] = getField(selection, variables);
@@ -71,23 +239,23 @@ const getSelections = (selections: any[]=[], variables: {}) => {
   return json;
 }
 
-const getDefinition = (definition: any, variables: {}) => {
+const getDefinition = (definition: Definition, variables: Variables = {}) => {
   let json = getSelections(definition.selectionSet.selections, variables);
-  const vars = getVariables(definition.variableDefinitions, variables);
-  if(!isEmpty(vars)) {
-    json["__variables"] = vars;
+  if(!isEmpty(variables)) {
+    json["__variables"] = variables;
   }
   return json;
 };
 
-const getQuery = (query: string, variables: {}={}): string => {
-  let ast = gql(query);
-  let json = {};
-  ast.definitions.forEach((definition) => {
+const getQuery = (query: string, variables: Variables={}): string => {
+  let document = (gql(query) as Document);
+  let json: any = {};
+  document.definitions.forEach((definition) => {
     if(definition.kind === "FragmentDefinition") {
-      json[`fragment ${definition.name.value} on ${definition.typeCondition.name.value}`] = getDefinition(definition, variables);
+      json[`fragment ${definition.name.value} on ${definition.typeCondition.name.value}`] = getDefinition(definition);
     } else if(definition.kind === "OperationDefinition") {
-      json[`${definition.operation}${definition.name ? " " + definition.name.value : ""}`] = getDefinition(definition, variables);
+      const vars = getVariables(definition.variableDefinitions, variables);
+      json[`${definition.operation}${definition.name ? " " + definition.name.value : ""}`] = getDefinition(definition, vars);
     }
   });
   return jsonToGraphQLQuery(json);
